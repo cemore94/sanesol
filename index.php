3c376c16567553e80731a57322e3281287da2b7c
Cristian More <cemore94@gmail.com>
2017-10-26 22:36:52 -0500

    estoy probando en git

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ echo "esto me gusta";
 echo "esto me gusta";
 echo "esto me gusta";
 echo "esto me gusta";
+echo "Va cayendo una lagrima en tu mejilla";
+echo "si no la secas el viento la secará";
+echo "Te amo y te lo digo yo salserón";
 echo "esto me gusta";
-echo "esto me gusta";
-echo "esto me gusta";
-echo "esto me gusta";
-echo "esto me gusta";
-echo "esto me gusta";
+echo "Soy muchacho provinciano";
+echo "Me levanto muy temprano";
 ?>
