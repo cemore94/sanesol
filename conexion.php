92f40c18ad40e8feca1f86f49fd2eaf562d07eba
Cristian More <cemore94@gmail.com>
2017-11-07 12:44:24 -0500

    dale viejo hazlo bien

--- conexion.php
+++ conexion.php
@@ -2,4 +2,9 @@
 $conex = mysqli_connect("localhost","root","alumno");
 echo "ohhhh como es la vida, pruebas y mas pruebas en code";
 echo " suavecito no te enfades"
+alksndalskmdalks
+fpassthrufs
+df
+asdgergregerger
+ergergerngerngrelngerlngoregmoiwneio nweofnowien oi
 ?>
